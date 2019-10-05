8c877f4070f8d85530ad5f441ea12eee8ac16a11
Michael Wildman <michael@mwild.me>
2019-10-05 21:49:58 +1300

    Clean up "tests".

--- test/Saunter.Tests/UnitTest1.cs
+++ test/Saunter.Tests/UnitTest1.cs
@@ -1,17 +1,14 @@
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using NUnit.Framework;
 using Saunter.AsyncApiSchema.v2_0_0;
-using Saunter.AsyncApiSchema.v2_0_0.Bindings.Amqp;
-using Saunter.Attributes;
-//using Saunter.Attributes.Bindings.Amqp;
 using Saunter.Generation;
 
 namespace Saunter.Tests
 {
+    /// <remarks>
+    /// These are not real tests! Tests will be added once the API is semi-stable...
+    /// </remarks>
     public class Tests
     {
         [SetUp]
@@ -67,87 +64,8 @@ namespace Saunter.Tests
             var provider = sp.GetRequiredService<IAsyncApiSchemaProvider>();
 
             var schema = provider.GetSchema();
-            var json = JsonConvert.SerializeObject(schema, Formatting.Indented, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-            Debug.WriteLine(json);
 
             Assert.That(schema, Is.Not.Null);
-
         }
-
-//        [Test]
-//        public void Test2()
-//        {
-//            var services = new ServiceCollection() as IServiceCollection;
-//            services.AddAsyncApiSchemaGeneration(
-//                options =>
-//                {
-//                    options.AsyncApiSchema = new AsyncApiSchema.v2_0_0.AsyncApiSchema();
-//                    options.AssemblyMarkerTypes.Add(this.GetType());
-//
-//                    AmqpDefaults.ChannelBinding.Is = AmqpChannelBindingIs.RoutingKey;
-//                    AmqpDefaults.ChannelBinding.Name = "accounts";
-//                    AmqpDefaults.ChannelBinding.ExchangeType = "topic";
-//                });
-//
-//            var sp = services.BuildServiceProvider();
-//
-//            var provider = sp.GetRequiredService<IAsyncApiSchemaProvider>();
-//            
-//            var schema = provider.GetSchema();
-//            
-//            var json = JsonConvert.SerializeObject(schema, Formatting.Indented, new JsonSerializerSettings
-//            {
-//                NullValueHandling = NullValueHandling.Ignore
-//            });
-//            Debug.WriteLine(json);
-//            
-//            Assert.That(schema.Channels, Is.Not.Null);
-//        }
-//    }
-
-    public class ExampleHeaders
-    {
-        public string MessageId { get; set; }
-    }
-    
-    public class ExampleMessage
-    {
-        public string SomeProperty { get; set; }
-
-        public int Abc { get; set; }
-    }
-
-    public interface IAmqpClient
-    {
-        void Publish(string topic, string routingKey, object payload);
-    }
-    
-//    [AsyncApi]
-//    public class MessagePublisher
-//    {
-//        private readonly IAmqpClient amqp;
-//
-//        public MessagePublisher(IAmqpClient amqp)
-//        {
-//            this.amqp = amqp;
-//        }
-//
-//    
-//        /// <summary>
-//        /// Should be able to pull this description for the channel
-//        /// </summary>
-//        [Channel("example.routing.key")]
-//        [AmqpChannelBinding]
-////        [PublishOperation]
-//        public void PublishExampleMessage2([Payload] ExampleMessage payload)
-//        {
-//            var exchangeName = MethodBase.GetCurrentMethod().GetCustomAttribute<AmqpChannelBindingAttribute>().Name;
-//            var routingKey = MethodBase.GetCurrentMethod().GetCustomAttribute<ChannelAttribute>().Name;
-//            
-//            amqp.Publish(exchangeName, routingKey, payload);
-//        }
     }
 }
