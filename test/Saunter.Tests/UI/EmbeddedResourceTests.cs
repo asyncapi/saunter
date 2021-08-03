4bb06acbbb263983722a5a1bc1a5499cd6b74dd1
Michael Wildman <michael@mwild.me>
2021-08-03 21:57:39 +1200

    Move nodejs stuff out of the csproj path. Unit tests for embedded resources.

---
+++ test/Saunter.Tests/UI/EmbeddedResourceTests.cs
@@ -0,0 +1,27 @@
+﻿using Saunter.UI;
+using Shouldly;
+using Xunit;
+
+namespace Saunter.Tests.UI
+{
+    public class EmbeddedResourceTests
+    {
+        [Fact]
+        public void SaunterBinaryContainsEmbeddedResourcesFromProject()
+        {
+            var resources = typeof(AsyncApiUiMiddleware).Assembly.GetManifestResourceNames();
+            resources.ShouldContain("Saunter.UI.index.html");
+        }
+
+        [Fact]
+        public void SaunterBinaryContainsEmbeddedResourcesFromNpm()
+        {
+            var err = "Execute `npm install` from the src/Saunter.UI directory to install the UI dependencies";
+
+            var resources = typeof(AsyncApiUiMiddleware).Assembly.GetManifestResourceNames();
+            resources.ShouldContain("Saunter.UI.default.min.css", customMessage: err);
+            resources.ShouldContain("Saunter.UI.index.js", customMessage: err);
+        }
+
+    }
+}
