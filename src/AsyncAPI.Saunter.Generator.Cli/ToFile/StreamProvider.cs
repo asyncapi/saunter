4f1064e412d07c4206cb2f135c61d4302cb84d6f
Senn Geerts <Senn.Geerts@belden.com>
2024-07-13 21:33:08 +0200

    #196 GetStreamFor try recreate directory??

--- src/AsyncAPI.Saunter.Generator.Cli/ToFile/StreamProvider.cs
+++ src/AsyncAPI.Saunter.Generator.Cli/ToFile/StreamProvider.cs
@@ -16,21 +16,21 @@ internal class StreamProvider(ILogger<StreamProvider> logger) : IStreamProvider
 
         if (!string.IsNullOrEmpty(path))
         {
-            var directory = Path.GetDirectoryName(path);
+            var directory = new DirectoryInfo(Path.GetDirectoryName(path));
             var sw = Stopwatch.StartNew();
             do
             {
                 try
                 {
-                    Directory.CreateDirectory(directory);
+                    directory.Create();
                 }
                 catch (Exception e) when (sw.Elapsed < TimeSpan.FromMilliseconds(250))
                 {
-                    logger.LogDebug(e, "Retry...");
+                    logger.LogDebug(e, $"Retry... {directory.Parent.Exists}, {directory.Parent.Parent.Exists}, {directory.Parent.Parent.Parent.Exists}");
                     Thread.Sleep(100);
                 }
             }
-            while (!Directory.Exists(directory));
+            while (!directory.Exists);
         }
 
         return path != null ? File.Create(path) : Console.OpenStandardOutput();
